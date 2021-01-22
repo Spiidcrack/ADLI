002f73440d1a7c8de782b7b31ad888e0a76acc03
Steve <Steve@DESKTOP-8B65UL5>
2021-01-22 14:01:48 +0100

    DateController & MongoRepo als leere Klassen hinzugefügt

---
+++ ADLBackEnd/src/main/java/de/volkswagen/f73/adlis/ADLBackEnd/Controller/ADLDataController.java
@@ -0,0 +1,13 @@
+package de.volkswagen.f73.adlis.ADLBackEnd.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import de.volkswagen.f73.adlis.ADLBackEnd.Repo.ADLMongoRepo;
+
+@RestController
+public class ADLDataController {
+	
+	// FillerName für Repo
+	private ADLMongoRepo repo;
+
+}
