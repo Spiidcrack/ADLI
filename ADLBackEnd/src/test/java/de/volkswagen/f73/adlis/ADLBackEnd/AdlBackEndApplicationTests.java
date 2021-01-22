ab406b49e83f1074b73b66b3328444ba2a69a63d
Steve <Steve@DESKTOP-8B65UL5>
2021-01-22 13:30:17 +0100

    Init ADLBackEnd

---
+++ ADLBackEnd/src/test/java/de/volkswagen/f73/adlis/ADLBackEnd/AdlBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package de.volkswagen.f73.adlis.ADLBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdlBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
